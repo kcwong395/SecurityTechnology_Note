053ec01e52f90afc9af2dbb3a531746533b18c17
kcwong <kcwong395@gmail.com>
2019-02-18 04:47:35 +0800

    separate the decryption to decrypt via brute force and with a known key

--- CaesarCipher/caesar_cipher_decrypt.py
+++ CaesarCipher/caesar_cipher_decrypt.py
@@ -1,24 +1,20 @@
-# This function intends to decrypt Caesar ciphertext by brute forcing all possible key
+# This function intends to decrypt plaintext with a known key
 
-cipherText = input("Please enter the ciphertext to be decoded: \n")
-
-
-def BruteForceAttack(cipherText):
-    for key in range(26):
-        for letter in cipherText:
-            remain = ord(letter)
-            if 'a' <= letter <= 'z':
-                remain -= key
-                if remain < ord('a'):
-                    remain += 26
-            elif 'A' <= letter <= 'Z':
-                remain -= key
-                if remain < ord('A'):
-                    remain += 26
-            print(chr(remain), end='')
-        print()
+plainText = input("Please enter the ciphertext to be decoded: \n")
+key = input("Please enter the key for decryption: \n")
 
 
-print("\nThe following are all the possible plaintexts")
-BruteForceAttack(cipherText)
+def CaesareEncrypt(plainText):
+    cipher = ""
+    for letter in plainText:
+        if 'a' <= letter <= 'z':
+            letter = chr((ord(letter) - int(key) - ord('a')) % 26 + ord('a'))
+        elif 'A' <= letter <= 'Z':
+            letter = chr((ord(letter) - int(key) - ord('A')) % 26 + ord('A'))
+        cipher += letter
+    return cipher
+
+
+print("The following is the decrypted message: ")
+print(CaesareEncrypt(plainText))
 a = input()
